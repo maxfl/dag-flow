3f9a20cc6c5a69d23e73e665932cf31e5439f030
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-09-14 15:52:44 +0300

    update

--- dagflow.py
+++ dagflow.py
@@ -1,10 +1,69 @@
-class Input(object):
-    _name  = '<undefined>'
-    _node  = None
-    _output = None
-    _corresponding_output = None
+from collections import OrderedDict
 
-    def __init__(self, name, node, corresponding_output=None):
+class Undefined(object):
+    def __init__(self, what):
+        self.what=what
+
+    def __str__(self):
+        return 'Undefined '+self.what
+
+undefinedname = Undefined('name')
+undefineddata = Undefined('data')
+undefineddatatype = Undefined('datatype')
+undefinednode = Undefined('node')
+undefinedgraph = Undefined('graph')
+undefinedoutput = Undefined('output')
+
+def iter_inputs(inputs):
+    if isinstance(inputs, Input):
+        yield inputs
+    else:
+        if isinstance(inputs, (tuple, list)):
+            iterable = inputs
+        elif isinstance(inputs, OrderedDict):
+            iterable = inputs.values()
+        elif isinstance(inputs, Node):
+            iterable = inputs.iter_inputs()
+        else:
+            raise Exception('Do not know how to iterate inputs')
+
+        for input in iterable:
+            yield input
+
+def iter_outputs(outputs):
+    if isinstance(outputs, Output):
+        yield outputs
+    else:
+        if isinstance(outputs, (tuple, list)):
+            iterable = outputs
+        elif isinstance(outputs, OrderedDict):
+            iterable = outputs.values()
+        elif isinstance(outputs, Node):
+            iterable = outputs.iter_outputs()
+        else:
+            raise Exception('Do not know how to iterate outputs')
+
+        for output in iterable:
+            yield output
+
+def rshift(outputs, inputs):
+    corresponding_outputs = tuple()
+    for output, input in zip(iter_outputs(outputs), iter_inputs(inputs)):
+        output.connect_to(input)
+        corresponding_outputs += input.corresponding_output(),
+
+    return corresponding_outputs
+
+def lshift(inputs, outputs):
+    return rshift(outputs, inputs)
+
+class Input(object):
+    _name  = undefinedname
+    _node  = undefinednode
+    _output = undefinedoutput
+    _corresponding_output = undefinedoutput
+
+    def __init__(self, name, node, corresponding_output=undefinedoutput):
         self._name = name
         self._node=node
         self._corresponding_output=corresponding_output
@@ -13,7 +72,7 @@ class Input(object):
         if not isinstance(output, Output):
             raise exception('Bad output type')
 
-        if self._output:
+        if self._output is not undefinedoutput:
             raise ConnectException('Output is already connected to the input')
 
         self._output = output
@@ -24,19 +83,25 @@ class Input(object):
     def data(self):
         return self._output.data()
 
+    def touch(self):
+        return self._output.touch()
+
     def datatype(self):
         return self._output.datatype()
 
     def corresponding_output(self):
         return self._corresponding_output
 
+    __lshift__  = lshift
+    __rrshift__ = lshift
+
 class Output(object):
-    _name   = '<undefined>'
-    _node  = None
+    _name   = undefinedname
+    _node   = undefinednode
     _inputs = None
 
-    _data   = None
-    _datatype = None
+    _data   = undefineddata
+    _datatype = undefineddatatype
 
     def __init__(self, name, node):
         self._name = name
@@ -53,16 +118,17 @@ class Output(object):
         self._inputs.append(input)
         input.set_output(self)
 
-    def __rshift__(self, input):
-        self.connect_to(input)
-        return input.corresponding_output()
-
-    def __rlshift__(self, input):
-        self.connect_to(input)
-        return input.corresponding_output()
+    __rshift__  = rshift
+    __rlshift__ = lshift
 
     def tainted(self):
         return self._node.tainted()
+
+    def taint(self):
+        return self._node.taint()
+
+    def touch(self):
+        return self._node.touch()
 
     def data(self):
         self._node.touch()
@@ -71,42 +137,99 @@ class Output(object):
     def datatype(self):
         return self._datatype
 
-from collections import OrderedDict
+    def iter_outputs(self):
+        yield self
+
 class Node(object):
-    _name = '<undefined>'
+    _name = undefinedname
     _inputs  = None
     _outputs = None
+    _graph   = undefinedgraph
+    _tainted = True
     _fcn     = None
-    _graph   = None
-    _tainted = true
+    _fcn_chain = None
 
-    def __init__(self, name, fcn, graph=None):
+    def __init__(self, name, fcn=lambda i, o, n: None, graph=undefinedgraph):
         self._name = name
         self._fcn = fcn
+        self._fcn_chain = []
         self._inputs = OrderedDict()
         self._outputs = OrderedDict()
         self._graph = graph
 
-    def _add_input(name, corresponding_output=None):
-        ret = Input(name, self, corresponding_output)
-        self._inputs[name] = ret
-        return ret
+    def _add_input(self, name, corresponding_output=undefinedoutput):
+        if name in self._inputs:
+            raise Exception('Input {node}.{input} already exist', node=self.name, input=name)
+        input = Input(name, self, corresponding_output)
+        self._inputs[name] = input
 
-    def _add_output(name):
-        ret = Output(name, self)
-        self._output[name] = ret
-        return ret
+        if self._graph is not undefinedgraph:
+            self._graph.add_input(input)
 
-    def _add_pair(iname, oname):
+        return input
+
+    def _add_output(self, name):
+        if name in self._outputs:
+            raise Exception('Output {node}.{output} already exist', node=self.name, output=name)
+
+        output = Output(name, self)
+        self._outputs[name] = output
+
+        if self._graph is not undefinedgraph:
+            self._graph.add_output(output)
+
+        return output
+
+    def _add_pair(self, iname, oname):
         output = self._add_output(oname)
         input = self._add_input(iname, output)
         return input, output
 
-    def touch(self):
-        if not self._tainted:
+    def _wrap_fcn(self, wrap_fcn, *other_fcns):
+        prev_fcn = self._fcn
+        self._fcn_chain.append(prev_fcn)
+        def wrapped_fcn(*args, **kwargs):
+            wrap_fcn(prev_fcn, *args, **kwargs)
+
+        self._fcn = wrapped_fcn
+
+        if other_fcns:
+            self._wrap_fcn(*other_fcns)
+
+    def _unwrap_fcn(self):
+        if not self._fcn_chain:
+            raise Exception('Unable to unwrap bare function')
+        self._fcn = self._fcn_chain.pop()
+
+    def iter_outputs(self):
+        for output in self._outputs.values():
+            yield output
+
+    def iter_inputs(self):
+        for input in self._inputs.values():
+            yield input
+
+    def touch(self, force=False):
+        if not self._tainted and not force:
             return
 
-        self._fcn()
+        self.eval()
+        self._tainted = False
+
+    def eval(self):
+        self._fcn(self._inputs, self._outputs, self)
+
+    def taint(self):
+        if self._tainted:
+            return
+
+        for output in self._outputs.values():
+            output.taint()
+
+    __rshift__  = rshift
+    __rlshift__ = rshift
+    __lshift__  = lshift
+    __rrshift__ = lshift
 
 class Graph(object):
     _nodes  = None
@@ -118,8 +241,14 @@ class Graph(object):
         self._inputs  = []
         self._outputs = []
 
-    def add_node(self, name, fcn):
+    def add_node(self, name, fcn=lambda i, o, n: None):
         newnode = Node(name, fcn, self)
         self._nodes.append(newnode)
         return newnode
+
+    def add_input(self, input):
+        self._inputs.append(input)
+
+    def add_output(self, output):
+        self._outputs.append(output)
 
--- test/test.py
+++ test/test.py
@@ -2,7 +2,103 @@
 
 from dagflow import *
 
-i = Input('input', None)
-o = Output('output', None)
+def test_01():
+    i = Input('input', None)
+    o = Output('output', None)
 
-o >> i
+    o >> i
+
+def test_02():
+    n1 = Node('node1')
+    n2 = Node('node2')
+
+    n1._add_output('o1')
+    n1._add_output('o2')
+
+    n2._add_input('i1')
+    n2._add_input('i2')
+    n2._add_output('o1')
+
+    n1 >> n2
+
+def test_03():
+    n1 = Node('node1')
+    n2 = Node('node2')
+
+    out = n1._add_output('o1')
+
+    n2._add_input('i1')
+    n2._add_output('o1')
+
+    out >> n2
+
+def test_04():
+    n1 = Node('node1')
+    n2 = Node('node2')
+
+    out = n1._add_output('o1')
+
+    n2._add_pair('i1', 'o1')
+
+    final = out >> n2
+
+def test_05():
+    n1 = Node('node1')
+    n2 = Node('node2')
+
+    out1 = n1._add_output('o1')
+    out2 = n1._add_output('o2')
+
+    _, final = n2._add_pair('i1', 'o1')
+    n2._add_input('i2')
+
+    (out1, out2) >> n2
+
+def test_06():
+    n1 = Node('node1')
+    n2 = Node('node2')
+
+    out1 = n1._add_output('o1')
+    out2 = n1._add_output('o2')
+
+    _, final = n2._add_pair('i1', 'o1')
+    n2._add_input('i2')
+
+    (out1, out2) >> n2
+
+    print(final.data())
+
+def test_07():
+    def printer(fcn, inputs, outputs, node):
+        print('Evaluate {node}'.format(node=node._name))
+        fcn(inputs, outputs, node)
+        print('    ... done')
+
+    def toucher(fcn, inputs, outputs, node):
+        for input in inputs.values():
+            input.touch()
+
+    n1 = Node('node1')
+    n2 = Node('node2')
+
+    n1._wrap_fcn(printer, toucher)
+    n2._wrap_fcn(printer, toucher)
+
+    out1 = n1._add_output('o1')
+    out2 = n1._add_output('o2')
+
+    _, final = n2._add_pair('i1', 'o1')
+    n2._add_input('i2')
+
+    (out1, out2) >> n2
+
+    print(final.data())
+    import IPython; IPython.embed()
+
+test_01()
+test_02()
+test_03()
+test_04()
+test_05()
+test_06()
+test_07()
